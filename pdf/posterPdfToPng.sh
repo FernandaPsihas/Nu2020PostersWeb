0e981bd71c3535a2477d9fc117b7802eca66b729
Alec Habig <ahabig@umn.edu>
2020-06-09 12:41:36 -0500

    add Marco's landscape posters.  Make small thumbnails wider for landscape

--- pdf/posterPdfToPng.sh
+++ pdf/posterPdfToPng.sh
@@ -6,4 +6,5 @@
 convert -density 150 -antialias -background white -flatten -append -resize 720x -quality 100 "$1"  "${1%.pdf}.png"
 
 # make a thumbnail for the index
-convert -density 150 -antialias -background white -flatten -append -resize 320x -quality 100 "$1" "${1%.pdf}-sm.png"
+# while portrait worked well at 320x, try 480x for landscape
+convert -density 150 -antialias -background white -flatten -append -resize 480x -quality 100 "$1" "${1%.pdf}-sm.png"
